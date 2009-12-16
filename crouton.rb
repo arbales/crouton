216663087efa9772b0c10673d30284d345789ce0
Austin Bales <arbales@gmail.com>
2009-12-16 01:54:11 -0500

    Modaulr style

--- crouton.rb
+++ crouton.rb
@@ -1,4 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 
-load 'models.rb'
+load 'models.rb' 
+
+class Application < Sinatra::Base
+  register Sinatra::Warden
+
+  get '/admin' do
+    authorize!('/login') # require session, redirect to '/login' instead of work
+    haml :admin
+  end
+
+  get '/dashboard' do
+    authorize! # require a session for this action
+    haml :dashboard
+  end
+end
--- models.rb
+++ models.rb
@@ -1,8 +1,10 @@
 require 'dm-core'
 
 class Person
+  include DataMapper::Resource
+  
   property :id, Serial
   property :name, Text
-  porperty :password_hash, Text
+  property :password_hash, Text
   property :password_salt, Text
 end
